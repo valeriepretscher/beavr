f35f711e07f6ec9663651ac7ffc8238f98634cee
Michael G. Ströck <michael@stroeck.com>
2016-01-27 13:51:34 +0100

    Start building out footer.

--- client/src/app/components/footer.js
+++ client/src/app/components/footer.js
@@ -8,26 +8,45 @@ export default React.createClass({
 
     render() {
         return (
-            <footer className="footer hidden-print">
-                <div className="container">
+            <footer className="hidden-print">
+                <div className="block app-block-footer">
+                  <div className="container">
                     <div className="row">
-
-                        <div className="content col-md-12 col-sm-12 text-center">
-                            <div>StarHackIt is the starting point to build a full stack web application.</div>
-                            <div>
-                                Get the source code at <a href="https://github.com/FredericHeem/starhackit" target="_blank">GitHub</a>
-                            </div>
-                        </div>
-
-                        <div className="col-md-5 col-sm-12 hidden-sm hidden-xs">
-                            <div className="pull-right">
-
-                            </div>
-                        </div>
+                      <div className="col-sm-2 m-b">
+                        <ul className="list-unstyled list-spaced">
+                          <li><h6 className="text-uppercase">Products</h6></li>
+                          <li>Todo</li>
+                          <li>Calendario</li>
+                          <li>Email Town</li>
+                          <li>Pomodorotary</li>
+                          <li>ChillTower</li>
+                        </ul>
+                      </div>
+                      <div className="col-sm-2 m-b">
+                        <ul className="list-unstyled list-spaced">
+                          <li><h6 className="text-uppercase">Extras</h6></li>
+                          <li>AutotuneU</li>
+                          <li>Freestyler</li>
+                          <li>Chillaxation</li>
+                        </ul>
+                      </div>
+                      <div className="col-sm-2 m-b">
+                        <ul className="list-unstyled list-spaced">
+                          <li><h6 className="text-uppercase">Support</h6></li>
+                          <li>Online Support</li>
+                          <li>Telephone Sales</li>
+                          <li>Help Desk</li>
+                          <li>Workshops</li>
+                        </ul>
+                      </div>
+                      <div className="col-sm-6">
+                        <h6 className="text-uppercase">About</h6>
+                        <p>Shoutout to Invision team for creating the <a href="http://www.invisionapp.com/do">Do UI kit</a> that we used to fake our app screenshots. Also to the Dribbble community for providing phone mockups that look amazing.</p>
+                      </div>
                     </div>
+                  </div>
                 </div>
             </footer>
         );
     }
-
 } );
