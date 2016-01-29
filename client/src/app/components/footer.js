4f4468ad48cc0ab7ed6bffa98b209ff3a636c3de
Michael G. Ströck <michael@stroeck.com>
2016-01-29 14:24:07 +0100

    Restyle footer.

--- client/src/app/components/footer.js
+++ client/src/app/components/footer.js
@@ -14,7 +14,7 @@ export default React.createClass({
                     <div className="row">
                       <div className="col-sm-2 m-b">
                         <ul className="list-unstyled list-spaced">
-                          <li><h6 className="text-uppercase">Products</h6></li>
+                          <li><h6 className="text-uppercase text-primary">Products</h6></li>
                           <li>Todo</li>
                           <li>Calendario</li>
                           <li>Email Town</li>
@@ -24,7 +24,7 @@ export default React.createClass({
                       </div>
                       <div className="col-sm-2 m-b">
                         <ul className="list-unstyled list-spaced">
-                          <li><h6 className="text-uppercase">Extras</h6></li>
+                          <li><h6 className="text-uppercase text-primary">Extras</h6></li>
                           <li>AutotuneU</li>
                           <li>Freestyler</li>
                           <li>Chillaxation</li>
@@ -32,7 +32,7 @@ export default React.createClass({
                       </div>
                       <div className="col-sm-2 m-b">
                         <ul className="list-unstyled list-spaced">
-                          <li><h6 className="text-uppercase">Support</h6></li>
+                          <li><h6 className="text-uppercase text-primary">Support</h6></li>
                           <li>Online Support</li>
                           <li>Telephone Sales</li>
                           <li>Help Desk</li>
@@ -40,8 +40,8 @@ export default React.createClass({
                         </ul>
                       </div>
                       <div className="col-sm-6">
-                        <h6 className="text-uppercase">About</h6>
-                        <p>Shoutout to Invision team for creating the <a href="http://www.invisionapp.com/do">Do UI kit</a> that we used to fake our app screenshots. Also to the Dribbble community for providing phone mockups that look amazing.</p>
+                        <h6 className="text-uppercase text-primary">About</h6>
+                        <p>Call us crazy, but we believe that there is a smarter way to match young enthusiastic job seekers with employers that are in need of qualified and reliable staff. Our mission is to build the fastest short-term job application platform, providing jobseekers with a simple and efficient solution to make extra cash as well as employers with the data and tools they need to hire applicants in a timely manner.</p>
                       </div>
                     </div>
                   </div>
