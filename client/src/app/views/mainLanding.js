aa528b69419d0ab301c254648d23aa48183defc7
Michael G. Ströck <michael@stroeck.com>
2016-01-30 22:13:23 +0100

    Keep working on layout and theme.

--- client/src/app/views/mainLanding.js
+++ client/src/app/views/mainLanding.js
@@ -28,210 +28,102 @@ export default React.createClass({
                         Smart, simple<br />
                         job-search.
                     </h1>
+                    <button className="btn btn-primary mt-30 btn-xl text-center m-5">Get started</button>
+
+                    <div className="masthead-scroll-arrow hidden-xs">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="37" height="16" viewBox="0 0 37 16">
+                                <path d="M19.075 15.43c-.35.366-.926.414-1.334.097L.76 2.08C.326 1.743.248 1.115.587.68.926.242 1.553.163 1.99.502L18.35 13.46 34.864.617c.436-.34 1.064-.26 1.403.175.34.436.26 1.065-.175 1.404L19.075 15.43z" fill="#CCD3DC" fill-rule="evenodd">
+                                </path>
+                            </svg>
+                        </span>
+                    </div>
                 </div>
             </div>
 
-            <div className="block block-lg pt-30 app-block-stats">
+            <div className="block block-lg app-block-profile">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-7 col-sm-6 text-center">
+                        <div className="col-md-7 col-sm-6 text-xs-center text-sm-left">
+                            <h1 className="block-title">Showcase your inner beast</h1>
+                        </div>
+                        <div className="col-md-5 col-sm-6 text-right">
                             <img src="../assets/img/seeker-profile-mirror.png" data-transition="entrance" />
-                            <hr className="m-t-0 m-b-lg m-x-auto visible-xs" />
-                        </div>
-                        <div className="col-md-5 col-sm-6 text-xs-center text-sm-left">
-                            <div className="row m-y-md">
-                                <h1 className="block-title pb-50 mt-0 pt-0 text-center">Showcase your inner beast</h1>
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Create</h2>
-                                        <p className="lead">your profile with a few clicks.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number">Show</h2>
-                                        <p className="lead">employers what you've truly got.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row hidden-sm">
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Collect</h2>
-                                        <p className="lead">all the information employers need in one place.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Display</h2>
-                                        <p className="lead">recommendations from your past employers &amp; colleagues.</p>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="block block-lg pt-0 pb-20 app-block-stats">
+            <div className="block block-lg app-block-race-car">
                 <div className="container">
-                    <h1 className="block-title pb-50 text-center">Apply instantly &mdash; fast &amp; easy</h1>
                     <div className="row">
-                        <div className="col-md-6 col-md-offset-1 col-sm-6 text-xs-center text-sm-left">
-                            <div className="row m-y-md">
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Discover</h2>
-                                        <p className="lead">unique opportunities near you.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number">Throw out</h2>
-                                        <p className="lead">Your CVs and cover letters - on beavr you won't need them..</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row hidden-sm">
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Apply</h2>
-                                        <p className="lead">with a single click.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">Arrive</h2>
-                                        <p className="lead">at your job with all the information you need to shine.</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="hidden-xs col-md-7 col-sm-6 text-xs-center text-sm-left">
+                            <img src="../assets/img/seeker-race-car.png" data-transition="entrance" />
                         </div>
-                        <div className="text-right app-block-racecar-img">
-                            <img src="../assets/img/seeker-race-car.png"  data-transition="entrance" />
+                        <div className="col-md-5 col-sm-6 pull-right">
+                             <h1 className="block-title">This is an awesome header</h1>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="block block-lg pt-0 app-block-stats">
+            <div className="block block-lg app-block-profile">
                 <div className="container">
-                    <h1 className="block-title pb-50 text-center">directly message your employer</h1>
                     <div className="row">
-                        <div className="col-md-7 col-sm-6 text-center">
+                        <div className="col-md-7 col-sm-6 text-xs-center text-sm-left">
+                            <h1 className="block-title">Showcase your inner beast</h1>
+                        </div>
+                        <div className="col-md-5 col-sm-6 text-right">
+                            <img src="../assets/img/seeker-profile-mirror.png" data-transition="entrance" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="block block-lg app-block-chat">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-7 col-sm-6 text-xs-center text-sm-left">
                             <img src="../assets/img/seeker-chat.png" data-transition="entrance" />
-                            <hr className="m-t-0 m-b-lg m-x-auto visible-xs" />
                         </div>
-                        <div className="col-md-5 col-sm-6 text-xs-center text-sm-left">
-                            <div className="row m-y-md">
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">chat</h2>
-                                        <p className="lead">directly within the app.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number">clarify</h2>
-                                        <p className="lead">any questions you might have beforehand</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row hidden-sm">
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">connect</h2>
-                                        <p className="lead">even before your first day has started</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">know</h2>
-                                        <p className="lead">exactly what lies ahead.</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="col-md-5 col-sm-6 pull-right">
+                             <h1 className="block-title">Start earning money with jobs you love</h1>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="block block-lg pt-30 pb-30 app-block-truck">
+            <div className="block block-lg app-block-truck">
                 <div className="container">
-                    <h1 className="block-title pb-50 text-center">start earning money with jobs you love</h1>
                     <div className="row">
-                        <div className="col-md-6 col-md-offset-1 col-offset-11 col-sm-6 text-xs-center text-sm-left">
-                            <div className="row m-y-md">
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">make</h2>
-                                        <p className="lead">your financial problems disappear.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number">gain</h2>
-                                        <p className="lead">valuable experience with every single job.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row hidden-sm">
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">discover</h2>
-                                        <p className="lead">the strengths that make you, you.</p>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6 m-b-lg">
-                                    <div className="statcard">
-                                        <h2 className="statcard-number block-title">become</h2>
-                                        <p className="lead">a beavr pro today and say goodbye to boring job platforms.</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="col-md-7 col-sm-6 text-xs-center text-sm-left">
+                            <h1 className="block-title">Showcase your inner beast</h1>
                         </div>
-                        <div className="col-md-5 col-sm-6 text-center">
+                        <div className="col-md-5 col-sm-6 text-right">
                             <img src="../assets/img/employer-ice-cream-truck.png" data-transition="entrance" />
-                            <hr className="m-t-0 m-b-lg m-x-auto visible-xs" />
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="block block-lg pt-30 app-block-stats">
+            <div className="block block-lg app-block-giraffe pt-30 pb-50 full-width">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-7 col-sm-6 text-center">
-                            <img src="../assets/img/seeker-giraffe.png" data-transition="entrance" />
-                            <hr className="m-t-0 m-b-lg m-x-auto visible-xs" />
+                        <div className="col-md-6 pl-50 pr-50 col-sm-6 text-center">
+                            <hr />
+                            <hr />
+                            <h1 className="block-title">Enjoy life!</h1>
+                            <hr />
+                            <h2>Travel the world, buy a laser sowrd, get your own place, fly to the moon, acquire a hover board...
+                                the possibilities are endless!</h2>
+                            <p>(and if you ever need to make more cash, we can help you out!)</p>
+                            <hr />
+                            <button className="btn btn-primary btn-xl text-center ">Get started</button>
                         </div>
-                        <div className="col-md-5 col-sm-6 text-xs-center text-sm-left">
-                            <div className="row m-y-md text-center">
-                                <h1 className="block-title">Enjoy life!</h1>
-                                <h2>travel the world, buy a laser sowrd, get your own place, fly to the moon, acquire a hover board...
-                                    the possibilities are endless!</h2>
-                                <p>(and if you ever need to make more cash, we can help you out!)</p>
-                                <button className="btn btn-primary mt-30 btn-lg text-center m-5">Get started</button>
-                            </div>
+                        <div className="col-md-6 col-sm-6 text-right">
+                            <img src="../assets/img/seeker-giraffe.png" data-transition="entrance" />
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <div className="block block-lg p-0 text-center">
-                <div className="container-fluid">
-                    <p className="lead m-b-md">
-                        Become a Beavr Pro now and <strong>be independent</strong> forever!
-                    </p>
-                    <form className="form-inline">
-                        <input className="form-control m-b m-5" placeholder="Email Address" />
-                        <input className="form-control m-b m-5" type="password" placeholder="Create a Password" />
-                        <button className="btn btn-primary m-5">Get started</button>
-                    </form>
-                    <small className="text-muted">
-                        By clicking "Get started" I agree to beavr's
-                        <a href="#">Terms of service</a>
-                    </small>
                 </div>
             </div>
 
